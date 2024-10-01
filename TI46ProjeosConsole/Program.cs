df056e35856d84617833d120b7c7a85ee8a38566
GIOVANNE SILVERIO MARCONDES <giovanne.smarcondes@senacsp.edu.br>
2024-09-30 21:29:24 -0300

    Pimeira versão do ProjetoTi46Console

--- TI46ProjeosConsole/Program.cs
+++ TI46ProjeosConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TI46ProjeosConsole
@@ -10,6 +11,10 @@ namespace TI46ProjeosConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ola mundo");
+            Console.WriteLine("teste, linha 2");
+
         }
+
     }
 }
